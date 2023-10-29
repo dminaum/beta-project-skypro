7645b80913141ca32f07986e20bb7d765c69f4f2
Dmitrii Naumov <dmi3naumov@gmail.com>
2023-10-30 00:33:28 +0300

    added masking account function

--- src/widget.py
+++ src/widget.py
@@ -3,3 +3,9 @@ def masking_card2(card_number: str) -> str:
         if s.isdigit():
             break
     return card_number[:i+4] + ' ' + card_number[i+5:i+7]  + '** **** ' + card_number[i+12:]
+
+def masking_account2(account_number: str) -> str:
+    for i, s in enumerate(account_number):
+        if s.isdigit():
+            break
+    return account_number[:5] + '**' + account_number[-4:]

d955b04925e94c0ec8b3456fc55872f780869a49
Dmitrii Naumov <dmi3naumov@gmail.com>
2023-11-04 21:09:33 +0300

    correcting grammar errors

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,14 @@
 from widget import formatting_time
 
 
-def filter_dicts_by_state(dicts: list[dict], state: str ="EXECUTED") -> list[dict]:
+def filter_dicts_by_state(dicts: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Фильтрует список словарей по значению ключа 'state'.
     :param dicts: Список словарей для фильтрации.
     :param state: Значение 'state', по которому производится фильтрация (по умолчанию 'EXECUTED').
     :return: Новый список, содержащий только словари с ключом 'state', равным заданному значению 'state'.
     """
-    return [dict_ for dict_ in dicts if dict_.get("state") == state]
+    return [d for d in dicts if d.get("state") == state]
 
 
 def filter_dicts_by_date(dicts: list[dict], reverse: bool = True) -> list[dict]:
